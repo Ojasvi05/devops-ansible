9441a9b5fa8a1c14e968bb6e36b3b6349ee39d97
Ojasvi05 <95043478+Ojasvi05@users.noreply.github.com>
2021-11-26 19:01:00 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,16 +1,15 @@
 provider "aws" {
- region = "us-east-2"
+region = "us-east-2"
 }
 resource "aws_instance" "myawsserver" {
-  ami ="ami-077e31c4939f6a2f3"
-  instance_type ="t2.small"
-  key_name ="os-import"
-  
-  tags {
-    Name = "Os-devops-Server"
-    env = "Dev"
+  ami = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.small"
+  key_name = "os-import"
+
+  tags = {
+    Name = "Ojasvi-devops-server"
+    env = "production"
   }
-  
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
   }
